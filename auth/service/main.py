6283583b3921937b0dd7a5a6fb222ab00f921a87
wood <amper.sil.tok@gmail.com>
2022-10-19 14:13:39 +0300

    поправлено до стандартной реализации фласка без uwsgi

---
+++ auth/service/main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from endpoints.hello import hello_page
+
+
+app = Flask(__name__)
+
+app.register_blueprint(hello_page)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
