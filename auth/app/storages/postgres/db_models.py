a739e312f678fcab40bcad233f62e5519f664cf3
wood <amper.sil.tok@gmail.com>
2022-10-20 13:30:49 +0300

    добавлено заполнение таблицы ролей при запуске приложения

--- auth/app/storages/postgres/db_models.py
+++ auth/app/storages/postgres/db_models.py
@@ -16,8 +16,9 @@ class User(db.Model):
     )
     login = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
-    role_id = db.Column(db.Integer, nullable=False, unique=True)
+    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"), nullable=False)
     r_jwt = db.Column(db.String, nullable=False)
+
     roles = db.relationship("Role")
 
     def __repr__(self):
@@ -26,14 +27,8 @@ class User(db.Model):
 
 class Role(db.Model):
     __tablename__ = "roles"
-    id = db.Column(
-        UUID(as_uuid=True),
-        primary_key=True,
-        default=uuid.uuid4,
-        unique=True,
-        nullable=False,
-    )
-    role_id = db.Column(db.Integer, db.ForeignKey("users.role_id"), nullable=False)
+
+    id = db.Column(db.Integer, primary_key=True)
     role = db.Column(db.String, nullable=False)
 
     def __repr__(self):
@@ -44,3 +39,11 @@ class Role(db.Model):
 init_db(app)
 app.app_context().push()
 db.create_all()
+# Создание ролей
+
+if Role.query.count() == 0:
+    follower = Role(id=1, role="follower")
+    subscriber = Role(id=2, role="subscriber")
+    db.session.add(follower)
+    db.session.add(subscriber)
+    db.session.commit()
