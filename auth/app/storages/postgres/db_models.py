a07e66cba090a5265a30666dda026bc29f257de2
wood <amper.sil.tok@gmail.com>
2022-10-21 07:09:01 +0300

    добавлены модели устройств

--- auth/app/storages/postgres/db_models.py
+++ auth/app/storages/postgres/db_models.py
@@ -20,6 +20,7 @@ class User(db.Model):
     email = db.Column(db.String, nullable=False)
 
     social = db.relationship("UserSocial")
+    device = db.relationship("UserDevice")
 
     def __repr__(self):
         return f"<User {self.login}>"
@@ -29,17 +30,34 @@ class UserSocial(db.Model):
     __tablename__ = "user_social"
 
     id = db.Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
-    client_id = db.Column(db.ForeignKey("users.id"), primary_key=True)
-    social_id = db.Column(db.ForeignKey("social.id"), primary_key=True)
+    user_id = db.Column(db.ForeignKey("users.id"), primary_key=True)
+    social_id = db.Column(db.ForeignKey("socials.id"), primary_key=True)
     url = db.Column(db.String)
     social = db.relationship("Social")
 
 
 class Social(db.Model):
-    __tablename__ = "social"
+    __tablename__ = "socials"
 
     id = db.Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
     name = db.Column(db.Integer, nullable=False)
+
+
+class UserDevice(db.Model):
+    __tablename__ = "user_device"
+
+    id = db.Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
+    user_id = db.Column(db.ForeignKey("users.id"), primary_key=True)
+    social_id = db.Column(db.ForeignKey("devices.id"), primary_key=True)
+    entry_time = db.Column(db.String)
+    social = db.relationship("Device")
+
+
+class Device(db.Model):
+    __tablename__ = "devices"
+
+    id = db.Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
+    device = db.Column(db.String, nullable=False)
 
 
 def init_tables(app):
