9fa0c368e1c45116c0d378e6b30cc5aeb5737392
wood <amper.sil.tok@gmail.com>
2022-10-25 17:30:57 +0300

    добавлена документация

--- auth/app/api/v1/profile.py
+++ auth/app/api/v1/profile.py
@@ -9,24 +9,32 @@ profile = Blueprint("profile", __name__, url_prefix="/api/v1")
 
 @profile.route("/profile", methods=["GET"])
 def user_full_information():
+    """Ендпоинт для запроса данных пользователя"""
+
     user_data = profile_service().get_all_user_info(request)
     return user_data
 
 
 @profile.route("/profile/devices", methods=["GET"])
 def user_device_history():
+    """Ендпоинт для запроса истории девайсов с которых была авторизация"""
+
     user_devices_data = profile_service().get_devices_user_history(request)
     return user_devices_data
 
 
 @profile.route("/profile/socials", methods=["GET"])
 def user_socials():
+    """Ендпоинт списка социальных сетей пользователя"""
+
     user_socials_data = profile_service().get_socials(request)
     return user_socials_data
 
 
 @profile.route("/profile/change/email", methods=["POST"])
 def change_user_email():
+    """Ендпоинт для изменения почтового адреса пользователя"""
+
     try:
         profile_service().change_email(request)
         return jsonify({"correct": "email changed"})
@@ -36,6 +44,7 @@ def change_user_email():
 
 @profile.route("/profile/change/password", methods=["POST"])
 def change_user_password():
+    """Ендпоинт для изменения пароля пользователя"""
     try:
         if profile_service().change_password(request):
             return jsonify({"correct": "password changed"})
