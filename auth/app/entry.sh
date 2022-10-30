cd7fa455951b8ef186bc5e27c83d03b27d776dd9
wood <amper.sil.tok@gmail.com>
2022-10-30 08:32:10 +0300

    добавлен вейтер в ентрипоинте

--- auth/app/entry.sh
+++ auth/app/entry.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+while ! nc -z -v postgres 5432; do
+      sleep 1
+    done
+
+while ! nc -z -v redis 6379; do
+      sleep 1
+    done
+
 cd /auth/app/
 alembic upgrade head
 uwsgi --http 0.0.0.0:5000 --master -p 4 -w wsgi:app
