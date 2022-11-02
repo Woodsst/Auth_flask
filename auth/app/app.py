0cda4a6b42128c5bb2dce26e191fd5d2ad8115af
wood <amper.sil.tok@gmail.com>
2022-11-02 12:57:39 +0300

    создание админа перенесено в фласк

--- auth/app/app.py
+++ auth/app/app.py
@@ -15,9 +15,21 @@ from api.v1.login import login_page
 from api.v1.profile import profile
 from api.v1.registration import registration_page
 from api.v1.tokens_work import tokens_work
+from superuser import add_admin
 
 app.register_blueprint(tokens_work)
 app.register_blueprint(registration_page)
 app.register_blueprint(login_page)
 app.register_blueprint(profile)
 app.register_blueprint(crud_pages)
+
+
+@app.cli.command("createadmin")
+def create_admin():
+    """Команда для добавления пользователя с ролью администратора"""
+
+    login = input("Name: ")
+    password = input("Password: ")
+    email = input("Email:")
+
+    add_admin(login, password, email)
--- auth/app/superuser.py
+++ auth/app/superuser.py
@@ -1,52 +1,30 @@
-import argparse
 import uuid
-from core.defaultrole import DefaultRole
-import psycopg2
+
+import sqlalchemy.exc
 from email_validator import validate_email
-from werkzeug.security import generate_password_hash
 
-from config.settings import default_settings
-
-
-def add_superuser(user_data: dict):
-    """Функция для добавления пользователя"""
-
-    con = psycopg2.connect(default_settings.postgres)
-    with con.cursor() as cur:
-        cur.execute(
-            """
-        INSERT INTO users (id, login, password, role, email)
-        VALUES (%(id)s, %(login)s, %(password)s, %(role)s, %(email)s)
-        """,
-            {
-                "id": str(uuid.uuid4()),
-                "login": user_data["login"],
-                "password": generate_password_hash(user_data["password"]),
-                "role": DefaultRole.ADMIN_KEY.value,
-                "email": user_data["email"],
-            },
-        )
-        con.commit()
+from core.defaultrole import DefaultRole
+from storages.db_connect import db
+from storages.postgres.db_models import User
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "createsuperuser",
-    help="Команда для создания пользователя с правами администратора",
-)
+def add_admin(login: str, password: str, email: str):
+    """Запись администратора в базу"""
 
-args = parser.parse_args()
-if args.createsuperuser:
-    name = input("Name: ")
-    password = input("Password: ")
-    email = input("Email:")
     validate_email(email)
     if len(password) < 8:
         raise ValueError("password too short")
-    user_data = {
-        "login": name,
-        "password": password,
-        "email": email,
-    }
-
-    add_superuser(user_data)
+    try:
+        user_id = uuid.uuid4()
+        user = User(
+            login=login,
+            password=password,
+            email=email,
+            id=user_id,
+            role=DefaultRole.ADMIN_KEY.value,
+        )
+        db.session.add(user)
+        db.session.commit()
+    except sqlalchemy.exc.IntegrityError:
+        db.session.rollback()
+        print("user exists")
